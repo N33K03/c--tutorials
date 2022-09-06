e1fce892bfef4359a10598997c09148d41dc029c
Neek kaviani <107409157+N33K03@users.noreply.github.com>
2022-09-06 15:35:39 +0100

    Add files via upload

---
+++ Orders of operations.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // ()
+    // * and /
+    // + and -
+
+    double x = (1 + 2) * 3;    //content in the brackets, then multiplication
+    cout << x;
+
+
+
+    return 0;
+}
