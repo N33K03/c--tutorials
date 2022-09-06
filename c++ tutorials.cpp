6d18899057c838cb0fa5d52e9724e6209e4abc85
Neek kaviani <107409157+N33K03@users.noreply.github.com>
2022-09-06 13:37:44 +0100

    Add files via upload

---
+++ c++ tutorials.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+    int main() {
+        // ()
+        // * and /
+        // + and -
+
+        double x = (1 + 2) * 3;    //content in the brackets, then multiplication
+        cout << x;
+
+    
+
+    return 0;
+}
+
+
+   
+
