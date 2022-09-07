227f95de9a6c5904f24037e3205f0c007806683a
Neek kaviani <107409157+N33K03@users.noreply.github.com>
2022-09-07 19:43:18 +0100

    Add files via upload

---
+++ Formatting console output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	int x = 10;
+	int y = 20;
+	std::cout << "x =" << x << std::endl //endl prints on next line
+	          << "y =" << y;  // y prints on next line
+	return 0;
+}
