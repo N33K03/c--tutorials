9ca5ae5563d4f6eb5cb046574e25f8ef1bb2de0c
Neek kaviani <107409157+N33K03@users.noreply.github.com>
2022-09-13 12:27:04 +0100

    Add files via upload

---
+++ Intializing Variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+    int main() {
+        double price = 99.99;
+        float interestRate = 3.67f;
+        long fileSize = 90000L;         // need suffex at the end of the value
+        char letter = 'a';
+        bool isValid = false;
+        // can use auto to infer type of variable
+
+        return 0;
+}
